6d6d02a4d050370e92bcbb2fb51d9e9f227ab4b2
fernando94 <lfmartinezpantoja@gmail.com>
2020-06-29 16:22:54 -0500

    full commit

---
+++ src/main/java/com/hiberus/config/HiberusConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.hiberus.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class HiberusConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiberusConfigServerApplication.class, args);
+	}
+
+
+
+}
